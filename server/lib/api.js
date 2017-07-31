621d06ccf4d2709b79d6ebe2bd84b22438bda74b
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2017-07-31 17:13:23 +0900

    add getRelevantSubmissions

--- server/lib/api.js
+++ server/lib/api.js
@@ -1,16 +1,48 @@
+const R = require('ramda');
 const fetch = require('node-fetch');
+const moment = require('moment');
+
+const list = require('./list.js').list;
 
 const endpoint = 'https://judgeapi.u-aizu.ac.jp';
+const maxProblemCount = 5000;
+// const dateStart = moment('2017-08-01');
+// const dateEnd = moment('2017-09-01');
+const dateStart = moment('2015-08-01');
+const dateEnd = moment('2017-09-01');
 
-// string -> string -> Promise Object
-function hitAPI(userId, problemId) {
+function validate(sub) {
+  const timestamp = parseInt(sub.submissionDate, 10);
+  return R.contains(sub.problemId, list) &&
+    moment(timestamp).isBetween(dateStart, dateEnd);
+}
+
+function extract(sub) {
+  return R.pick([
+    'judgeId',
+    'userId',
+    'problemId',
+    'submissionDate',
+    'status',
+  ], sub);
+}
+
+function hitAPI(url) {
+  return fetch(url).then(res => res.json());
+}
+
+function getAllSubmissions(userId) {
   const url =
-    `${endpoint}/submission_records/users/${userId}/problems/${problemId}`;
+    `${endpoint}/submission_records/users/${userId}?size=${maxProblemCount}`;
+  return hitAPI(url)
+    .then(R.map(extract));
+}
 
-  return fetch(url)
-    .then(res => res.json());
+function getRelevantSubmissions(userId) {
+  return getAllSubmissions(userId)
+    .then(R.filter(validate));
 }
 
 module.exports = {
-  hitAPI,
+  getRelevantSubmissions,
 };
